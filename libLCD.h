3f6e88abba5dcf4d6d28764479003cb580d510e4
Bruno DELATTRE <delattreb@gmail.com>
2018-06-15 22:01:32 +0200

    Ok for release

--- libLCD.h
+++ libLCD.h
@@ -5,11 +5,10 @@
 //
 // Version: 1.0.0
 
-//#include <LiquidCrystal_I2C.h>
-#include "Libraries/NewliquidCrystal/LiquidCrystal_I2C.h"
+#include <LiquidCrystal_I2C.h>
 #include "var.h"
 
-LiquidCrystal_I2C lcdi2c(LCD_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C lcdi2c(LCD_ADDR, LCD_ROW, LCD_LINE);
 
 class libLCD {
 
@@ -31,6 +30,7 @@ public:
 	void begin()
 	{
 		lcdi2c.begin(LCD_ROW, LCD_LINE);
+		lcdi2c.backlight();
 	}
 
 	void displayText() {
