4b49165e4207b4e336ad241c5568ca1d8ffe71be
Bruno DELATTRE <delattreb@gmail.com>
2018-11-11 18:03:04 +0100

    Add Custom caracter

--- libLCD.h
+++ libLCD.h
@@ -9,6 +9,36 @@
 #include "var.h"
 
 LiquidCrystal_I2C lcdi2c(LCD_ADDR, LCD_ROW, LCD_LINE);
+byte wifiChar[] = {
+  B01110,
+  B10001,
+  B00100,
+  B01010,
+  B00000,
+  B00100,
+  B00000,
+  B00000
+};
+byte mqttChar[] = {
+  B10000,
+  B10000,
+  B10100,
+  B10100,
+  B10101,
+  B10101,
+  B10101,
+  B00000
+};
+byte degreChar[] = {
+ B00110,
+  B01001,
+  B01001,
+  B00110,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
 
 class libLCD {
 
@@ -31,6 +61,9 @@ public:
 	{
 		lcdi2c.begin(LCD_ROW, LCD_LINE);
 		lcdi2c.backlight();
+		lcdi2c.createChar(0, wifiChar);
+		lcdi2c.createChar(1, mqttChar);
+		lcdi2c.createChar(2, degreChar);
 	}
 
 	void displayText() {
@@ -40,13 +73,15 @@ public:
 		lcdi2c.setCursor(x, 1);
 		lcdi2c.print("Hum");
 
+		lcdi2c.setCursor(9, 0);
+		lcdi2c.write(2);
 		lcdi2c.setCursor(10, 0);
 		lcdi2c.print("C");
 		lcdi2c.setCursor(8, 1);
 		lcdi2c.print("%");
 
-		lcdi2c.setCursor(14, 0);
-		lcdi2c.print("W");
+		//lcdi2c.setCursor(14, 0);
+		//lcdi2c.print("W");
 		lcdi2c.setCursor(14, 1);
 		lcdi2c.print("Db");
 	}
@@ -64,14 +99,40 @@ public:
 	}
 
 	//
-	// displayWiFi connexion
+	// displayWiFi connexion KO
 	//
-	void displayWiFiConnexion(String connexion)
+	void displayWiFiKo()
 	{
-		lcdi2c.setCursor(15, 0);
-		lcdi2c.print(connexion);
+		lcdi2c.setCursor(14, 0);
+		lcdi2c.print("!");
 	}
 
+	//
+	// displayWiFi connexion OK
+	//
+	void displayWiFiOk()
+	{
+		lcdi2c.setCursor(14, 0);
+		lcdi2c.print(lcdi2c.write(0));
+	}
+
+	//
+	// displayMQTT connexion KO
+	//
+	void displayMQTTKo()
+	{
+		lcdi2c.setCursor(15, 0);
+		lcdi2c.print("!");
+	}
+
+	//
+	// displayMQTT connexion OK
+	//
+	void displayMQTTOk()
+	{
+		lcdi2c.setCursor(15, 0);
+		lcdi2c.print(lcdi2c.write(1));
+	}
 	//
 	// displayWiFiDbSignal
 	//
